c8e088e15f1179d34aea0aa91ccc7a7aa9d91236
Iiro Jäppinen <iiro@jappinen.fi>
2019-05-10 08:10:34 +0300

    test(google): test for window title

--- test/integration/google.spec.ts
+++ test/integration/google.spec.ts
@@ -9,10 +9,7 @@ describe("Google", (): void => {
 
   it("should contain the Google logo", async (): Promise<void> => {
     await browser.url("https://www.google.com");
-    const logo = await browser.$("img[alt=Google]");
-    await logo.waitForExist();
-    expect(await logo.getProperty("src")).toEqual(
-      "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-    );
+    const title = await browser.getTitle();
+    expect(title).toEqual("Google");
   });
 });
